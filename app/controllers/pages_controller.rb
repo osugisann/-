b607fb992464dd29be80d47c66e54c642e0d69f4
wonghockchuansugianto <sugi@gengosha.co.jp>
2021-01-23 20:58:21 +0900

    pp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
   def index; end
+
+  def privacy_policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  get 'privacy_policy', to: 'pages#privacy_policy'
 end
